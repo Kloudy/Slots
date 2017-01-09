3738854171cec94ba1bec704b22a79878f9d9cb7
Kloudy <kloudy@antarescraft.com>
2017-01-08 20:48:48 -0700

    -

---
+++ src/com/antarescraft/kloudy/slots/PromoTask.java
@@ -0,0 +1,16 @@
+package com.antarescraft.kloudy.slots;
+
+import com.antarescraft.kloudy.hologuiapi.guicomponents.GUIPage;
+
+/*
+ * Fetches promo gui page from url
+ */
+public class PromoTask 
+{
+	private static final String promoUrl = "";
+	
+	public static GUIPage fetchPromoGUIPage()
+	{
+		return null;
+	}
+}
---
+++ src/com/antarescraft/kloudy/slots/pagemodels/PromoPageModel.java
@@ -0,0 +1,15 @@
+package com.antarescraft.kloudy.slots.pagemodels;
+
+import org.bukkit.entity.Player;
+
+import com.antarescraft.kloudy.hologuiapi.HoloGUIPlugin;
+import com.antarescraft.kloudy.hologuiapi.guicomponents.GUIPage;
+import com.antarescraft.kloudy.hologuiapi.playerguicomponents.PlayerGUIPageModel;
+
+public class PromoPageModel extends PlayerGUIPageModel
+{
+	public PromoPageModel(HoloGUIPlugin plugin, GUIPage guiPage, Player player)
+	{
+		super(plugin, guiPage, player);
+	}
+}
